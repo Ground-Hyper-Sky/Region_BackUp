84384a2ee7bc4e8053467fa0358361e11bd953a1
mc_doge_ <wangyaocing@outlook.com>
2024-01-28 00:20:06 +0800

    Complete init
    Complete init

--- region_backup/__init__.py
+++ region_backup/__init__.py
@@ -1,9 +1,33 @@
+from mcdreforged.api.all import *
+
 PLUGIN_METADATA = {
-    'id': 'hello_world',
+    'id': 'restart_plugin',
     'version': '1.0.0',
-    'name': 'My Hello World Plugin'
+    'name': 'Restart Plugin',
+    'description': 'A simple plugin for restarting the server.',
+    'author': 'mc_doge_'
 }
 
+class RestartPlugin:
 
-def on_load(server, old):
-    server.logger.info('Hello world!')
+    def __init__(self, server: PluginServerInterface, config: dict):
+        self.server = server
+        self.server.register_command(
+            Literal('!!restart').runs(self.restart_server)
+        )
+
+    def on_load(self):
+        self.server.logger.info('Restart Plugin loaded.')
+
+    def on_unload(self):
+        self.server.logger.info('Restart Plugin unloaded.')
+
+    def restart_server(self, source: CommandSource):
+        # 直接调用 MCDR 提供的 restart 方法进行服务器重启
+        if not self.server.get_instance().restart():
+            self.server.say('Failed to restart the server. The server may already be stopped.')
+        else:
+            self.server.say('Server is restarting. Please reconnect later.')
+
+def on_load(server: PluginServerInterface, old):
+    return RestartPlugin(server, old)
