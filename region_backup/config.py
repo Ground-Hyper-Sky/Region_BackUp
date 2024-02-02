53dd398426c713a3566aefb505004ce7695dd4e8
GHS_Official <84139787+GHS-Official@users.noreply.github.com>
2024-02-03 02:19:41 +0800

    Update config.py
    更新了插件config文件

--- region_backup/config.py
+++ region_backup/config.py
@@ -1,4 +1,5 @@
 from mcdreforged.api.utils.serializer import Serializable
+from typing import Dict
 
 
 class rb_info(Serializable):
@@ -10,3 +11,18 @@ class rb_info(Serializable):
     command: str = ""
     comment: str = ""
 
+
+class rb_config(Serializable):
+    backup_path: str = "./rb_multi"
+    world_path: str = "./server/world"
+    minimum_permission_level: Dict[str, int] = {
+        "make": 1,
+        "pos_make": 1,
+        "back": 2,
+        "del": 2,
+        "confirm": 1,
+        "abort": 1,
+        "reload": 2,
+        "list": 0
+    }
+    slot: int = 5
